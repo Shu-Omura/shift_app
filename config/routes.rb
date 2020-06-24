13f367681a267ec8deb205d82f019919520eac74
Shu Oomura <ohs6261@gmail.com>
2020-06-25 01:58:32 +0900

    Create signin and signup form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
+    passwords: 'users/passwords',
   }
   resources :users, only: [:show, :index]
 end
