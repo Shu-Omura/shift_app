eebe8671b013e10e61d4f89fa3a6d5a4c64054b5
Shu Oomura <ohs6261@gmail.com>
2020-06-28 04:55:44 +0900

    Add original validation for collected_shift

--- app/models/collected_shift.rb
+++ app/models/collected_shift.rb
@@ -1,6 +1,16 @@
 class CollectedShift < ApplicationRecord
   belongs_to :user
-  validates_presence_of :started_at, :finished_at
+  validates_presence_of :started_at
+  validates_presence_of :finished_at
   validate :validates_datetime
-  validate :validates_datetime_format
+
+  private
+    def validates_datetime
+      if started_at.nil? || started_at < Date.today
+        errors.add(:started_at, '今日以降の日時を選択してください')
+      end
+      if finished_at.nil? || finished_at < started_at + 1.minute
+        errors.add(:finished_at, '開始時刻より後の日時を選択してください')
+      end
+    end
 end
