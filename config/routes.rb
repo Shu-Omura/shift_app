c6653936b06a261584f9358ec4f52848d6ad5d47
Shu Oomura <ohs6261@gmail.com>
2020-06-28 14:31:54 +0900

    Change routes for collected_shifts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'collected_shifts/index'
-  get 'collected_shifts/show'
-  get 'collected_shifts/new'
-  get 'collected_shifts/edit'
   root 'statics#top'
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
@@ -12,4 +8,5 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks',
   }
   resources :users, only: [:show, :index]
+  resources :collected_shifts
 end
