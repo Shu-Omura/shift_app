963bb69e9f6687c605982fb44db7518b6c16d797
Shu Oomura <ohs6261@gmail.com>
2020-06-28 17:22:38 +0900

    Create show and index

--- app/controllers/collected_shifts_controller.rb
+++ app/controllers/collected_shifts_controller.rb
@@ -1,8 +1,11 @@
 class CollectedShiftsController < ApplicationController
   def index
+    @users = User.where(company_id: current_user.company_id)
+    @collected_user_shifts = CollectedShift.where(user: current_user)
   end
 
   def show
+    @collected_shift = CollectedShift.find(params[:id])
   end
 
   def new
