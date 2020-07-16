ebf1777db0b4cee008c1192cb35023b0058e80df
Shu Oomura <ohs6261@gmail.com>
2020-07-16 20:09:48 +0900

    Fix google oauth using active-session_store

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 gem 'jquery-rails', '~> 4.4'
 gem 'rails-i18n', '~> 6.0'
 gem 'simple_calendar', '~> 2.4', '>= 2.4.1'
+gem 'activerecord-session_store'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_133906) do
+ActiveRecord::Schema.define(version: 2020_07_16_100925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2020_07_12_133906) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "collected_shift_id"
     t.index ["collected_shift_id"], name: "index_created_shifts_on_collected_shift_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
