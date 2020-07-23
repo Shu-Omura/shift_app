3edc82f756547f85685304e41a26d98588bb23b2
Shu Oomura <ohs6261@gmail.com>
2020-07-23 22:55:17 +0900

    Fix selenium_chrome settings

--- spec/support/selenium_chrom.rb
+++ spec/support/selenium_chrom.rb
@@ -1,12 +1,14 @@
 require 'capybara/rspec'
 require 'selenium-webdriver'
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, {
-    browser: :chrome,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      chrome_options: { args: %w(no-sandbox headless disable-gpu window-size=1680,1050) }
-    ),
-  })
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
-Capybara.javascript_driver = :selenium
+Capybara.javascript_driver = :selenium_chrome_headless
